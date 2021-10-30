32fb3f0af5c2735036922f7d5ebe9972acfdcc9d
hushendian <hushendian@comisys.net>
2021-10-30 13:51:08 +0800

    sss

---
+++ src/main/java/com/smallsoho/mcplugin/image/utils/CompressUtil.kt
@@ -0,0 +1,44 @@
+package com.smallsoho.mcplugin.image.utils
+
+import java.io.File
+
+class CompressUtil {
+
+    companion object {
+        private const val TAG = "Compress"
+        fun compressImg(imgFile: File) {
+            if (!ImageUtil.isImage(imgFile)) {
+                return
+            }
+
+            val oldSize = imgFile.length()
+            var newSize = 0L
+            if (ImageUtil.isJPG(imgFile)) {
+                var tempFilePath: String = "${imgFile.path.substring(0, imgFile.path.lastIndexOf("."))}_temp" +
+                        "${imgFile.path.substring(imgFile.path.lastIndexOf("."))}"
+                Tools.cmd("guetzli", "${imgFile.path} ${tempFilePath}")
+                val tempFile: File = File(tempFilePath)
+                newSize = tempFile.length()
+                println("newSize = ${newSize}")
+                if (newSize < oldSize) {
+                    var imgFileName: String = imgFile.path
+                    if (imgFile.exists()) {
+                        imgFile.delete()
+                    }
+                    tempFile.renameTo(File(imgFileName))
+                } else {
+                    if (tempFile.exists()) {
+                        tempFile.delete()
+                    }
+                }
+
+            } else {
+                Tools.cmd("pngquant", "--skip-if-larger --speed 1 --nofs --strip --force --output ${imgFile.path} -- ${imgFile.path}")
+                newSize = File(imgFile.path).length()
+            }
+
+            LogUtil.log(TAG, imgFile.path, oldSize.toString(), newSize.toString())
+        }
+    }
+
+}
